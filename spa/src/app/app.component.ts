4fe5d1513e56cf5944474cd41fb485d0eb546645
studds <daniel.studds@gmail.com>
2019-04-02 14:46:33 +1100

    Hooking up very simply connection between spa & sam

--- spa/src/app/app.component.ts
+++ spa/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'spa';
+    title = 'spa';
+    message$: Observable<Object>;
+    constructor(private httpClient: HttpClient) {
+        this.message$ = this.httpClient.get('/api/hello', {
+            responseType: 'json'
+        });
+    }
 }
---
+++ spa/src/proxy.conf.js
@@ -0,0 +1,7 @@
+module.exports = {
+    '/api': {
+        changeOrigin: true,
+        secure: false,
+        target: 'http://localhost:3000'
+    }
+};
